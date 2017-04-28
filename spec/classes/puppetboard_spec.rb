c8ee134f7c1aab2419a1bf2c9aacada999389c1a
Benjamin Merot <bem.its@dsg.dk>
2017-04-28 11:47:31 +0200

    Fixed spec resource name. Extend resource test coverage

--- spec/classes/puppetboard_spec.rb
+++ spec/classes/puppetboard_spec.rb
@@ -2,16 +2,17 @@ require 'spec_helper'
 describe 'puppetboard' do
   context 'with default values for all parameters' do
     let(:facts) { {:ipaddress_lo => '127.0.0.1'} }
-    let(:params) { { 'manage_supervisord' => true } }
     it { should contain_exec('generate_supervisor_conf') }
     it { should contain_file('/etc/init.d/supervisord') }
+    it { should contain_file('/etc/supervisord.d') }
+    it { should contain_file('/var/log/supervisor') }
     it { should contain_package('gcc') }
     it { should contain_package('gevent').with_provider('pip') }
     it { should contain_package('gunicorn').with_provider('pip') }
     it { should contain_package('puppetboard').with_provider('pip') }
     it { should contain_package('python2-pip') }
     it { should contain_package('supervisor').with_provider('pip') }
-    it { should contain_sevice('puppetboard') }
-    it { should contain_sevice('supervisord') }
+    it { should contain_service('puppetboard') }
+    it { should contain_service('supervisord') }
   end
 end
