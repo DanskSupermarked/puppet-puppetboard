e2b06145a7b8e76f8f56de4b55c5f3fc35465abd
Benjamin Merot <bem.its@dsg.dk>
2017-07-26 13:51:55 +0200

    Created a sub group for rspec testing of the config class

--- spec/classes/puppetboard_spec.rb
+++ spec/classes/puppetboard_spec.rb
@@ -2,10 +2,8 @@ require 'spec_helper'
 describe 'puppetboard' do
   context 'with default values for all parameters' do
     let(:facts) { {:ipaddress_lo => '127.0.0.1'} }
-    it { should contain_exec('generate_supervisor_conf') }
     it { should contain_file('/etc/init.d/supervisord') }
     it { should contain_file('/etc/supervisord.d') }
-    it { should contain_file('/var/log/supervisor') }
     it { should contain_package('gcc') }
     it { should contain_package('gevent').with_provider('pip') }
     it { should contain_package('gunicorn').with_provider('pip') }
@@ -14,5 +12,13 @@ describe 'puppetboard' do
     it { should contain_package('supervisor').with_provider('pip') }
     it { should contain_service('puppetboard') }
     it { should contain_service('supervisord') }
+
+    it { should contain_class('puppetboard::config') }
+
+    describe 'puppetboard::config' do
+      it { should contain_exec('generate_supervisor_conf') }
+      it { should contain_file('/var/log/supervisor') }
+    end
+
   end
 end
